d03249c7acf40f21d3e06344be14e8b33351b062
rafi1341 <131365258+rafi1341@users.noreply.github.com>
2025-12-10 00:36:13 +0600

    save

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-const startBtn = document.getElementById("start-btn");
-const tabs = document.querySelector(".tabs");
 const tabBtns = document.querySelectorAll(".tab-btn");
 const screens = document.querySelectorAll(".screen");
-
-// Launch app
-startBtn.addEventListener("click", () => {
-    document.querySelector(".banner").classList.add("hidden");
-    document.querySelector(".start-container").classList.add("hidden");
-    tabs.classList.remove("hidden");
-    document.getElementById("home").classList.remove("hidden"); // Show default screen
-});
 
 // Tab switching
 tabBtns.forEach(btn => {
