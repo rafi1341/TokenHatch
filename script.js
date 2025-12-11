6ff912dfb8a98d4881659bf1bdf1e208a230e82d
rafi1341 <131365258+rafi1341@users.noreply.github.com>
2025-12-11 10:02:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
             btn.classList.add("active");
         });
     });
+    const pb = new PocketBase('http://127.0.0.1:8090');
 
     // --- Egg tapping logic ---
     const egg = document.getElementById("egg");
